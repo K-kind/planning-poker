814942775d5ba41c8ad5fada48e94e5dfd3b7cf0
Keita Katahira <katahirakeita1201@gmail.com>
2022-12-11 16:11:45 +0900

    Update: card colors

--- src/features/rooms/components/TwoSidedCard.tsx
+++ src/features/rooms/components/TwoSidedCard.tsx
@@ -25,16 +25,18 @@ export const TwoSidedCard = ({ number, isOpen }: Props) => {
 
 const MyCard = ({ number, isOpen }: Props) => (
   <Box
-    sx={() => ({
+    sx={(theme) => ({
       borderWidth: 1,
       borderStyle: number === null ? "dashed" : "solid",
-      borderColor: "gray",
+      borderColor:
+        number === null ? theme.colors.gray[5] : theme.colors.gray[6],
       borderRadius: 4,
+      backgroundColor:
+        number === null || isOpen ? undefined : theme.colors.gray[4],
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
     })}
-    bg={number === null || isOpen ? undefined : "rgba(0, 0, 0, .12)"}
     w={50}
     h={70}
   >
