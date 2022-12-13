0bfce7b11a8056089f22b88fb2b326440748092c
Keita Katahira <katahirakeita1201@gmail.com>
2022-12-13 23:33:30 +0900

    Update: RoomHistories from List to Table

--- src/features/rooms/components/RoomHistories.tsx
+++ src/features/rooms/components/RoomHistories.tsx
@@ -1,6 +1,6 @@
 import { useContext, useMemo } from "react";
 import { Link } from "react-router-dom";
-import { List, Text } from "@mantine/core";
+import { Box, Table, Text } from "@mantine/core";
 import { useRooms } from "@/features/rooms/api/getRooms";
 import { AuthContext } from "@/providers/auth";
 
@@ -11,20 +11,40 @@ export const RoomHistories = () => {
   const rooms = useMemo(() => {
     return (roomsQuery.data ?? []).map((room) => ({
       id: room.id,
+      name: room.name,
       path: `/rooms/${room.id}`,
-      createdAt: new Date(room.createdAt).toLocaleString(),
+      createdAt: new Date(room.createdAt).toLocaleDateString(),
     }));
   }, [roomsQuery.data]);
 
+  if (rooms.length === 0) {
+    return <Text size="sm">過去に参加した部屋はありません。</Text>;
+  }
+
   return (
-    <List>
-      {rooms.map((room) => (
-        <List.Item key={room.id} lh={2}>
-          <Link to={room.path}>
-            <Text>作成日: {room.createdAt}</Text>
-          </Link>
-        </List.Item>
-      ))}
-    </List>
+    <Table striped>
+      <thead>
+        <tr>
+          <th>部屋名</th>
+          <th>作成日</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rooms.map((room) => (
+          <tr key={room.id}>
+            <td>
+              <Box
+                component={Link}
+                to={room.path}
+                sx={{ display: "block", width: "100%", height: "100%" }}
+              >
+                {room.name}
+              </Box>
+            </td>
+            <td>{room.createdAt}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 };
