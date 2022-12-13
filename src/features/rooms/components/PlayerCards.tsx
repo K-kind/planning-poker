2da67373ec0dde1784cfa8afd6b1d8f1068e4af9
Keita Katahira <katahirakeita1201@gmail.com>
2022-12-13 19:35:21 +0900

    Add: player name edit form

--- src/features/rooms/components/PlayerCards.tsx
+++ src/features/rooms/components/PlayerCards.tsx
@@ -1,7 +1,8 @@
-import { Flex, Sx, Text, useMantineTheme } from "@mantine/core";
+import { Flex, Sx } from "@mantine/core";
 import { Player } from "@/features/rooms/types/player";
 import { Room } from "@/features/rooms/types/room";
 import { TwoSidedCard } from "@/features/rooms/components/TwoSidedCard";
+import { PlayerName } from "@/features/rooms/components/PlayerName";
 
 type Props = {
   room: Room;
@@ -11,26 +12,13 @@ type Props = {
 
 export const PlayerCards = ({ room, player, sx }: Props) => {
   const isOpen = room.cardStatus === "open";
-  const theme = useMantineTheme();
 
   return (
     <Flex justify="space-around" columnGap={6} rowGap={8} wrap="wrap" sx={sx}>
       {room.players.map((p) => (
         <Flex key={p.id} direction="column" align="center" w={126}>
           <TwoSidedCard number={p.number} isOpen={isOpen} />
-          <Text
-            size="sm"
-            mt={4}
-            c={p.id === player.id ? theme.primaryColor : undefined}
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              lineHeight: "22px",
-              height: "calc(22px * 2)",
-            }}
-          >
-            {p.name}
-          </Text>
+          <PlayerName room={room} player={p} isMe={p.id === player.id} />
         </Flex>
       ))}
     </Flex>
---
+++ src/features/rooms/components/PlayerName.tsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { Button, Flex, Popover, Text, TextInput } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { Player } from "@/features/rooms/types/player";
+import { Room } from "@/features/rooms/types/room";
+import { useUpdatePlayer } from "@/features/rooms/api/updatePlayer";
+import { playerFormSchema } from "@/features/rooms/schemas/playerForm";
+import { useNotification } from "@/shared/hooks/useNotification";
+
+type Props = {
+  room: Room;
+  player: Player;
+  isMe: boolean;
+};
+
+export const PlayerName = ({ room, player, isMe }: Props) => {
+  if (isMe) {
+    return <WithPopover room={room} player={player} />;
+  } else {
+    return <NameText player={player} isMe={false} />;
+  }
+};
+
+const NameText = ({ player, isMe }: { player: Player; isMe: boolean }) => {
+  return (
+    <Text size="sm" mt={4} c={isMe ? "blue" : undefined}>
+      {player.name}
+    </Text>
+  );
+};
+
+type FormValues = { name: string };
+
+const WithPopover = ({ room, player }: { room: Room; player: Player }) => {
+  const { notifySuccess } = useNotification();
+  const form = useForm<FormValues>({
+    initialValues: { name: player.name },
+    validate: zodResolver(playerFormSchema().pick({ name: true })),
+  });
+
+  const [opened, setOpened] = useState(false);
+
+  const toggle = (open: boolean) => {
+    if (!open) {
+      form.clearErrors();
+    } else {
+      form.setValues({ name: player.name });
+    }
+    setOpened(open);
+  };
+
+  const updatePlayerMutation = useUpdatePlayer({ id: player.id, room });
+  const handleSubmit = async ({ name }: FormValues) => {
+    try {
+      await updatePlayerMutation.mutate({ name });
+    } catch (e) {
+      console.error(e);
+    }
+
+    notifySuccess({ message: "変更を保存しました。" });
+    toggle(false);
+  };
+
+  return (
+    <Popover opened={opened} onChange={toggle} trapFocus withArrow shadow="md">
+      <Popover.Target>
+        <span onClick={() => toggle(true)} style={{ cursor: "pointer" }}>
+          <NameText player={player} isMe={true} />
+        </span>
+      </Popover.Target>
+
+      <Popover.Dropdown
+        sx={(theme) => ({
+          background:
+            theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+        })}
+      >
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Flex align="center">
+            <TextInput size="xs" w="400" {...form.getInputProps("name")} />
+            <Button
+              type="submit"
+              size="xs"
+              ml="xs"
+              loading={updatePlayerMutation.isLoading}
+              loaderPosition="center"
+            >
+              保存
+            </Button>
+          </Flex>
+        </form>
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
