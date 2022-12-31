de80a4b63f17eb706ced5f21e97e601877fa22c5
Keita Katahira <katahirakeita1201@gmail.com>
2022-12-31 20:42:12 +0900

    Add: player last access times to SettingsDrawer

---
+++ src/features/rooms/components/PlayerList.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { Button, Flex, List, Popover, Text, Tooltip } from "@mantine/core";
+import { IconLogout } from "@tabler/icons";
+import styled from "@emotion/styled";
+import { Room } from "@/features/rooms/types/room";
+import { useNotification } from "@/shared/hooks/useNotification";
+import { useModal } from "@/shared/hooks/useModal";
+import { Player } from "@/features/rooms/types/player";
+import { useDeletePlayer } from "@/features/rooms/api/deletePlayer";
+
+type Props = {
+  room: Room;
+  me: Player;
+};
+
+export const PlayerList = ({ room, me }: Props) => {
+  const { notifySuccess } = useNotification();
+  const { confirm } = useModal();
+  const deletePlayerMutation = useDeletePlayer({ room });
+
+  const [poptargetPlayerId, setPoptargetPlayerId] = useState<
+    string | undefined
+  >();
+
+  const handleExit = async (player: Player) => {
+    const message =
+      player.id === me.id
+        ? "退室しますか？"
+        : `${player.name}さんを退室させますか？`;
+    const confirmed = await confirm({ message });
+    if (!confirmed) return;
+
+    try {
+      await deletePlayerMutation.mutateAsync({ id: player.id });
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+
+    notifySuccess({ message: "変更を保存しました。" });
+  };
+
+  return (
+    <List spacing="xs" withPadding>
+      {room.players.map((player) => (
+        <StyledListItem key={player.id}>
+          <Flex justify="space-between" align="center">
+            <Popover
+              position="bottom-start"
+              withArrow
+              shadow="md"
+              opened={poptargetPlayerId === player.id}
+            >
+              <Popover.Target>
+                <Text
+                  onMouseEnter={() => setPoptargetPlayerId(player.id)}
+                  onMouseLeave={() => setPoptargetPlayerId(undefined)}
+                  sx={{ flexGrow: 1 }}
+                >
+                  {player.name}
+                </Text>
+              </Popover.Target>
+              <Popover.Dropdown sx={{ pointerEvents: "none" }}>
+                <Text size="sm">
+                  最終アクセス: {player.lastAccessedAt.toLocaleString()}
+                </Text>
+              </Popover.Dropdown>
+            </Popover>
+
+            <Tooltip
+              label={player.id === me.id ? "退室する" : "退室させる"}
+              withArrow
+            >
+              <Button
+                variant="subtle"
+                size="xs"
+                color="red"
+                onClick={() => handleExit(player)}
+              >
+                <IconLogout />
+              </Button>
+            </Tooltip>
+          </Flex>
+        </StyledListItem>
+      ))}
+    </List>
+  );
+};
+
+const StyledListItem = styled(List.Item)`
+  & .mantine-List-itemWrapper {
+    width: calc(100% - 32px);
+  }
+  & > span {
+    width: 100%;
+  }
+`;
--- src/features/rooms/components/RoomSettingsDrawer.tsx
+++ src/features/rooms/components/RoomSettingsDrawer.tsx
@@ -1,24 +1,12 @@
 import { useEffect } from "react";
-import {
-  Button,
-  Divider,
-  Drawer,
-  Flex,
-  List,
-  Text,
-  TextInput,
-  Tooltip,
-} from "@mantine/core";
+import { Button, Divider, Drawer, Flex, TextInput } from "@mantine/core";
 import { useForm, zodResolver } from "@mantine/form";
-import { IconLogout } from "@tabler/icons";
-import styled from "@emotion/styled";
 import { Room } from "@/features/rooms/types/room";
 import { roomFormSchema } from "@/features/rooms/schemas/roomForm";
 import { useUpdateRoom } from "@/features/rooms/api/updateRoom";
 import { useNotification } from "@/shared/hooks/useNotification";
-import { useModal } from "@/shared/hooks/useModal";
 import { Player } from "@/features/rooms/types/player";
-import { useDeletePlayer } from "@/features/rooms/api/deletePlayer";
+import { PlayerList } from "@/features/rooms/components/PlayerList";
 
 type FormValues = { name: string; cards: string };
 
@@ -36,7 +24,6 @@ export const RoomSettingsDrawer = ({
   closeDrawer,
 }: Props) => {
   const { notifySuccess } = useNotification();
-  const { confirm } = useModal();
   const form = useForm<FormValues>({
     initialValues: { name: "", cards: "" },
     validate: zodResolver(roomFormSchema().pick({ name: true, cards: true })),
@@ -69,26 +56,6 @@ export const RoomSettingsDrawer = ({
 
     notifySuccess({ message: "変更を保存しました。" });
     doClose();
-  };
-
-  const deletePlayerMutation = useDeletePlayer({ room });
-
-  const handleExit = async (player: Player) => {
-    const message =
-      player.id === me.id
-        ? "退室しますか？"
-        : `${player.name}さんを退室させますか？`;
-    const confirmed = await confirm({ message });
-    if (!confirmed) return;
-
-    try {
-      await deletePlayerMutation.mutateAsync({ id: player.id });
-    } catch (e) {
-      console.error(e);
-      return;
-    }
-
-    notifySuccess({ message: "変更を保存しました。" });
   };
 
   return (
@@ -133,37 +100,7 @@ export const RoomSettingsDrawer = ({
         labelProps={{ size: "md" }}
       />
 
-      <List spacing="xs" withPadding>
-        {room.players.map((player) => (
-          <StyledListItem key={player.id}>
-            <Flex justify="space-between" align="center">
-              <Text>{player.name}</Text>
-              <Tooltip
-                label={player.id === me.id ? "退室する" : "退室させる"}
-                withArrow
-              >
-                <Button
-                  variant="subtle"
-                  size="xs"
-                  color="red"
-                  onClick={() => handleExit(player)}
-                >
-                  <IconLogout />
-                </Button>
-              </Tooltip>
-            </Flex>
-          </StyledListItem>
-        ))}
-      </List>
+      <PlayerList room={room} me={me} />
     </Drawer>
   );
 };
-
-const StyledListItem = styled(List.Item)`
-  & .mantine-List-itemWrapper {
-    width: calc(100% - 32px);
-  }
-  & > span {
-    width: 100%;
-  }
-`;
