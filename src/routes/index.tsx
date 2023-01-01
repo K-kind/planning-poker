81e46e7c56e91f7aa79b2cd47c641a23e0bf7bf9
Keita Katahira <katahirakeita1201@gmail.com>
2023-01-02 00:54:12 +0900

    Add: terms page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import { NotFound } from "@/routes/NotFound";
 import { MainLayout } from "@/shared/components/MainLayout";
 import { SetAuth } from "@/shared/components/SetAuth";
 import { PageLoader } from "@/shared/components/PageLoader";
+import { TermsPage } from "@/features/static/routes/TermsPage";
 
 const AppWithLayout = () => {
   return (
@@ -25,6 +26,7 @@ export const AppRoutes = () => {
       <Route path="/" element={<AppWithLayout />}>
         <Route index element={<NewRoom />} />
         <Route path="/rooms/:id" element={<RoomPage />} />
+        <Route path="/terms" element={<TermsPage />} />
         <Route path="/*" element={<NotFound />} />
       </Route>
     </Routes>
